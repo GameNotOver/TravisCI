b81762e5ffe1beaf5fe302bc30a2706c52368bb1
wuyuhang <wuyuhang@mail.com>
2021-07-15 16:33:15 +0800

    update runtest.sh by typing

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -1,5 +1,9 @@
-gcc -E callback.c > callback.i
-gcc –S callback.i –o callback.s
-gcc –c callback.s -o callback.o
+gcc -E ../callback.c > callback.i
+sleep 1s
+gcc -S callback.i -o callback.s
+sleep 1s
+gcc -c callback.s -o callback.o
+sleep 1s
 gcc callback.o -o callback
+sleep 1s
 ./callback
