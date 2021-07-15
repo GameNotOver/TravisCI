fb3bfb641d1793fb0ccc0ad08daeafc1d9b6909e
wuyuhang <wuyuhang@mail.com>
2021-07-15 16:41:18 +0800

    update runtest.sh modified callback.c

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -1,9 +1,5 @@
-gcc -E ../callback.c > callback.i
-sleep 1s
+gcc -E callback.c > callback.i
 gcc -S callback.i -o callback.s
-sleep 1s
 gcc -c callback.s -o callback.o
-sleep 1s
 gcc callback.o -o callback
-sleep 1s
 ./callback
